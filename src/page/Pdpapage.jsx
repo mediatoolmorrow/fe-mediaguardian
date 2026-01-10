61cc7b57e935f283df5952c56a36d91b7b71b2e6
Dimsumcatto <96424191+Chakhriya-c@users.noreply.github.com>
2026-01-10 13:54:11 +0700

    id 1 fix pdpa font size

--- src/page/Pdpapage.jsx
+++ src/page/Pdpapage.jsx
@@ -10,7 +10,7 @@ function Pdpapage() {
                     PDPA
                 </h1>
                 <div className="w-full flex-1 min-h-0 overflow-y-auto text-sm sm:text-base mb-3 sm:mb-6 px-1">
-                    <p className="text-gray-700 leading-relaxed">
+                    <p className="text-gray-700 leading-relaxed text-xl">
                         Lorem ipsum dolor sit amet consectetur. Adipiscing quis sit gravida a et enim iaculis aliquam ultrices. Lorem vitae nec mi convallis mi. Sed aenean vitae et dictumst lacus elementum egestas eu urna. Amet turpis felis praesent vitae urna et faucibus. Urna dolor turpis pellentesque ut nisi molestie egestas aliquet. Proin enim pharetra imperdiet amet duis ac orci ac tortor. Phasellus augue diam malesuada nisi ut enim sagittis. Luctus molestie volutpat orci praesent a tortor pellentesque in malesuada. Eu sit a arcu hac
                         <br /><br />
                         faucibus amet mollis.Lorem ipsum dolor sit amet consectetur. Adipiscing quis sit gravida a et enim iaculis aliquam ultrices. Lorem vitae nec mi convallis mi. Sed aenean vitae et dictumst lacus elementum egestas eu urna. Amet turpis felis praesent vitae urna et faucibus. Urna dolor turpis pellentesque ut nisi molestie egestas aliquet. Proin enim pharetra imperdiet amet duis ac orci ac tortor. Phasellus augue diam malesuada nisi ut enim sagittis. Luctus molestie volutpat orci praesent a tortor pellentesque in malesuada. Eu sit a arcu hac faucibus amet mollis.
