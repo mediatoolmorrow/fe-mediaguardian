7c309a570dc3aed8922c14956f87bf90f7cd3452
Dimsumcatto <96424191+Chakhriya-c@users.noreply.github.com>
2026-01-10 13:54:51 +0700

    issue 3 Navbar Size Fix

--- src/components/Pageframe/Navbar.jsx
+++ src/components/Pageframe/Navbar.jsx
@@ -8,10 +8,10 @@ export default function Navbar(){
     return (
         <div className="max-w-screen max-h-[115px] max-h-[80px] bg-navbar flex justify-between py-4 px-6 sm:px-16 rounded-none sm:rounded-3xl sm:rounded-b">
             <div className="flex h-[53px] gap-3 justify-center">
-                <img src="/favicon.svg" /> 
+                <img src="/favicon.svg" className="scale-125"/> 
                 <div className="flex flex-col -space-y-1 text-start justify-center"> 
-                    <p className="font-bold text-sm sm:text-base "> ชุมชนเฝ้าระวังสื่อ </p>
-                    <p className="font-light text-sm">  Media Guardians </p>
+                    <p className="font-bold text-base sm:text-lg "> ชุมชนเฝ้าระวังสื่อ </p>
+                    <p className="font-light text-base">  Media Guardians </p>
                 </div>
             </div>
             <div className="">
