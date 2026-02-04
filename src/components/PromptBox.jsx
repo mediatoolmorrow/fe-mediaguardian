2c740c62a406d8ce2ffa6d97ad6b302c05ee18f2
Dimsumcatto <96424191+Chakhriya-c@users.noreply.github.com>
2026-02-05 02:21:31 +0700

    bring back link content

--- src/components/PromptBox.jsx
+++ src/components/PromptBox.jsx
@@ -22,13 +22,11 @@ export default function PromptBox({
       displayName: "Text Content",
       icon: "/icon/message.svg",
     },
-    /*
     {
       id: "link",
       displayName: "Link Content",
       icon: "/icon/link.svg",
     },
-    */
     {
       id: "image",
       displayName: "Image Content",
